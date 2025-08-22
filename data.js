eec5d62b29f600acd25aaa27fb71ec6bf56d4501
priyankakumari997328-cmyk <priyankakumari997328@gmail.com>
2025-08-22 15:33:09 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,25 @@
+
+const user = [
+    {
+        Name: "peter",
+        age: 30,
+        email: "flana@gmail.com",
+    },
+    {
+        Name: "peter",
+        age: 30,
+        email: "flana@gmail.com",
+    },
+    {
+        Name: "peter",
+        age: 30,
+        email: "flana@gmail.com",
+    },
+    {
+        Name: "peter",
+        age: 30,
+        email: "flana@gmail.com",
+    }
+];
+
+module.exports = user;
