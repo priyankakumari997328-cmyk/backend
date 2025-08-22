1dd285155fb2b02113f36ffd2db51d9ec84a00e9
priyankakumari997328-cmyk <priyankakumari997328@gmail.com>
2025-08-22 15:37:17 +0530

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,31 @@
+const signup=[
+    {
+        username:"pihu",
+        email:"priyanka9977@gmail.com",
+        password:1234,
+
+
+    },
+    {
+        username:"pihu",
+        email:"priyanka9977@gmail.com",
+        password:1234,
+
+
+    },
+    {
+        username:"pihu",
+        email:"priyanka9977@gmail.com",
+        password:1234,
+
+
+    },
+    {
+        username:"pihu",
+        email:"priyanka9977@gmail.com",
+        password:1234,
+
+
+    },
+]
+module.exports = {signup};
